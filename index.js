da7e41fd467be991d5502c3df37e54ab129fd41e
Conrad Kambi <kambiconrad@gmail.com>
2023-09-19 11:15:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+// Test case: multiply
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+
+// Test case: random
+let random = 1
+Math.floor(Math.random() * 100) + 1;
+
+// Test case: mod
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+
+// Test case: max
+let max = 20
+Math.max([5, 10, 20])
+
+
+
